70e468cd555feeaefe969ca6b5c15226209af244
Benjamin Brink <kappa@dekka.com>
2013-01-10 09:32:51 -0800

    more data model blending

--- sql/postgresql/affiliates-create.sql
+++ sql/postgresql/affiliates-create.sql
@@ -6,6 +6,7 @@
 -- aka company_credits
 CREATE TABLE aff_rewards (
    contact_id integer,
+   
    --tx_time
    --reward_amt
    --conversion
@@ -20,4 +21,13 @@ CREATE TABLE aff_referrals (
    referring_type varchar(12),
    referral_calc_ref varchar(12),
    referring_info text
+-- from referral_calendar
+   time_to_credit timestamptz,
+   amount_to_credit numeric,
+ -- credited_p was referral_calender.active, which means?
+ -- There are cases where invoice_no not null while active=t
+   credited_p varchar(1),
+   invoice_no varchar(80)
 );
+
+
